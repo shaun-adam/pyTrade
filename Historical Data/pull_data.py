722e7e82166ced9b45dbeb1de8517e9039afe384
Shaun Storbakken <shaun.storbakken@gmail.com>
2022-05-06 21:54:52 -0600

    Refeshed using size 200 batches

--- Historical Data/pull_data.py
+++ Historical Data/pull_data.py
@@ -103,24 +103,24 @@ except Exception as err:# requests.exceptions.Timeout as err:
 
 mergeData = "DELETE FROM TSX where ROWID IN (SELECT F.ROWID FROM TSX F JOIN TMP T WHERE F.Ticker = T.Ticker and F.Date = T.Date)"
 insData = "INSERT INTO TSX SELECT Ticker, Date,Open ,High , Low , Close, AdjClose, Volume FROM TMP"
+tickers = [item for item in tickers if len(item)<8]
 
 totTickers = len(tickers)
 
 i = 0
-for ticks in chunks(tickers,100):
+for ticks in chunks(tickers,200):
     s = ' '
     s = s.join(ticks)
     ticks = s
-    df = yf.download(ticks,period = "max",progress=False)
+    df = yf.download(ticks,period = "1mo",progress=False,threads=True)
     df = df.rename(columns={"Adj Close": "AdjClose"})
     df=df.stack(level = 1).reset_index().rename(columns={"level_1":"Ticker"})
     conn_insert_df('TMP',df,db,'replace')
     conn_exec(db,mergeData)
     conn_exec(db,insData)
-    i=i+100
+    i=i+200
     print(i,"out of ",totTickers," checked")
 
+#figure out what's going on with git
 
-#split large list into bulks of 20
-#reshape DF to match that of SQL table
-#compare performance for 100 stocks - same time period (top 100 from list or something)
+#resample to weekly to determine long or netural list
