23f4e0a35a840280e854b68ca20875d59d34700d
Shaun Storbakken <shaun.storbakken@gmail.com>
2022-05-13 23:44:21 -0600

    created loop for pandas_ta

--- screeners.py
+++ screeners.py
@@ -1,9 +1,12 @@
 import pull_data as pu
+import pandas_ta as ta
 
 df = pu.getDF('2021-01-01',period = 'D', ticker = ['SU.TO','ABX.TO'])
 dfWeekly = pu.getDF('2021-01-01',period = 'W', ticker = ['SU.TO','ABX.TO'])
 
-print(df.head())
-print(df.shape)
-print(dfWeekly.head())
-print(dfWeekly.shape)
+for Date, df2 in dfWeekly.groupby(level=0):
+    df2 = df2.ta.macd(close='Adj_Close')
+    print(df2)
+
+#need to loop through weekly dataframes, calculate macdhistograms and check for trend
+#reread trading for a living section on the impulse strategy
